79655e7abf6734a344abce309b4b10f3543abced
Balaji431 <122263119+Balaji431@users.noreply.github.com>
2025-12-18 10:31:33 +0530

    Add files via upload

---
+++ components/Color.kt
@@ -0,0 +1,21 @@
+package com.example.mindcare.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurplePrimary = Color(0xFF7C3AED)
+val PurpleSecondary = Color(0xFF8B5CF6)
+val BlueAccent = Color(0xFF38BDF8)
+val TealAccent = Color(0xFF14B8A6)
+
+val GradientStart = Color(0xFF4C1D95)
+val GradientEnd = Color(0xFF0EA5E9)
+
+val SurfaceLight = Color(0xFFF7F5FF)
+val OnSurfaceLight = Color(0xFF111827)
+val CardLight = Color(0xFFFFFFFF)
+
+val SurfaceDark = Color(0xFF020617)
+val OnSurfaceDark = Color(0xFFE5E7EB)
+val CardDark = Color(0xFF020617)
+
+
